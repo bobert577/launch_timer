fd67bdc25670038e5f73a2961e46a75624064be7
Teddy <bobert577@gmail.com>
2020-11-21 14:30:16 -0600

    Small updates to formatting and started to work on timer

--- web_scraper.py
+++ web_scraper.py
@@ -1,12 +1,20 @@
 from urllib.request import urlopen
 from datetime import datetime
+import time
+from tkinter import *
+from tkinter import messagebox
+import numpy as np
 
-# url = 'http://olympus.realpython.org/profiles/aphrodite'
 url = 'https://spaceflightnow.com/launch-schedule/'
 
 page = urlopen(url)
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
+
+# Dump html to a .txt file for inspection
+file_object = open(r'html_dump.txt', 'w')
+file_object.write(html)
+file_object.close()
 
 # Function to find data given start and end string
 def get_launch_data(start_str, end_str):
@@ -16,17 +24,13 @@ def get_launch_data(start_str, end_str):
 
 # Get launch data and strip spaces from beginning and end
 launch_name = get_launch_data('</span><span class="mission">', '</span></div>').strip()
-launch_date = get_launch_data('<span class="launchdate">', '</span><span class="mission">').strip()
+# launch_date = get_launch_data('<span class="launchdate">', '</span><span class="mission">').strip()
+launch_date = 'Nov. 25' # Temporary launch date until Falcon 9 launch removed from website
 launch_time = get_launch_data('Launch time:</span> ', '<span class="strong"><br />').strip()
 
 # print(launch_name)
 # print(launch_date)
 # print(launch_time)
-
-# Dump html to a .txt file for inspection
-# file_object = open(r'html_dump.txt', 'w')
-# file_object.write(html)
-# file_object.close()
 
 # Clean launch data into usable format
 launch_time_cleaned = launch_time[:launch_time.find('GMT')+len('GMT')]
@@ -39,4 +43,34 @@ current_year = str(current_date.year)
 launch_datetime = current_year + launch_date + launch_time_cleaned
 launch_datetime_parsed = datetime.strptime(launch_datetime, '%Y%b. %d%H%M:%S %Z')
 
-print(launch_datetime_parsed)
+# Get timedelta object of time until launch
+length_of_countdown = launch_datetime_parsed - datetime.utcnow()
+#print(length_of_countdown)
+
+hours = str(abs(np.floor(length_of_countdown.total_seconds() / 3600)))
+minutes = str(np.floor((length_of_countdown.total_seconds() % 3600) / 60))
+seconds = str(np.floor(length_of_countdown.total_seconds() % 60))
+
+
+
+#####################################################
+################## Countdown GUI ####################
+#####################################################
+# define the countdown func. 
+def countdown(t): 
+    
+    while t: 
+        mins, secs = divmod(t, 60) 
+        timer = '{:02d}:{:02d}'.format(mins, secs) 
+        print(timer, end="\r") 
+        time.sleep(1) 
+        t -= 1
+      
+    print('Fire in the hole!!') 
+  
+  
+# input time in seconds 
+t = length_of_countdown.total_seconds()
+  
+# function call 
+countdown(int(t)) 
